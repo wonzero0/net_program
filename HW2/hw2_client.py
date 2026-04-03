0a98b216adfe16e69db97eca9acf952047c2bac2
Wonyoung Yeom <won00@sch.ac.kr>
2026-04-03 16:44:14 +0900

    hw2

---
+++ HW2/hw2_client.py
@@ -0,0 +1,17 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+addr = ('localhost', 9000)
+sock.connect(addr)
+
+msg = sock.recv(1024)
+print(msg.decode())
+
+name = "wonyoung Yeom"
+sock.send(name.encode())
+
+id_data = sock.recv(4)
+student_id = int.from_bytes(id_data, 'big')
+print(student_id)
+
+sock.close()
---
+++ HW2/hw2_server.py
@@ -0,0 +1,18 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 9000))
+s.listen(2)
+
+while True:
+    client, addr = s.accept()
+    print('Connection from ', addr)
+    client.send(b'Hello ' + addr[0].encode())
+
+    name_data = client.recv(1024)
+    print(name_data.decode())
+
+    student_id = 20231312
+    client.send(student_id.to_bytes(4, 'big'))
+
+    client.close()
